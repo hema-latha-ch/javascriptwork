a5614d4779c9abd4597a14c8950480f2b146129c
hemalatha chintalapally <hemalatha.chintalapally@gmail.com>
2020-05-10 00:25:11 -0500

    my initial checkin

---
+++ hello-world-program/firstprogram.js
@@ -0,0 +1,19 @@
+window.addEventListener("load",start, false);
+function start()
+{
+	document.getElementById("submitbutton").addEventListener("click", displayMessage, false);
+	
+}
+function displayMessage()
+{
+	document.write("Welcome to programming world");
+}
+
+
+
+
+
+
+
+
+
