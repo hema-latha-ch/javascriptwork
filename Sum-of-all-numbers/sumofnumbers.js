1a975d736822711773c33d57694a7affda781084
hemalatha chintalapally <hemalatha.chintalapally@gmail.com>
2020-05-11 18:46:54 -0500

    updated subtration multiplication and division

--- Sum-of-all-numbers/sumofnumbers.js
+++ Sum-of-all-numbers/sumofnumbers.js
@@ -1,14 +1,39 @@
 window.addEventListener("load", initializeButtons, false);
 function initializeButtons()
 {
-    document.getElementById("sumbutton").addEventListener("click",addition,false);
+    document.getElementById("addButton").addEventListener("click",addition,false);
+    document.getElementById("subButton").addEventListener("click", subtraction,false);
+    document.getElementById("mulButton").addEventListener("click",multiplication,false);
+    document.getElementById("divButton").addEventListener("click",division,false);
 }
 
 function addition()
 {
-  var firstNumber = parseInt(document.getElementById("firstnumber").value);//parse to integer, parseInt is a method, which will take the string as an input and convert them to integer.
-  var secondNumber = parseInt(document.getElementById("secondnumber").value);
+  var firstNumber = parseInt(document.getElementById("firstnumberTextbox").value);//parse to integer, parseInt is a method, which will take the string as an input and convert them to integer.
+  var secondNumber = parseInt(document.getElementById("secondnumberTextbox").value);
   var sum = firstNumber + secondNumber;
-  document.getElementById("sumnumber").value = sum.toString();
+  document.getElementById("resultTextboxid").value = sum.toString();
 }
 
+function subtraction()
+{
+  var firstNumber = parseInt(document.getElementById("firstnumberTextbox").value);//parse to integer, parseInt is a method, which will take the string as an input and convert them to integer.
+  var secondNumber = parseInt(document.getElementById("secondnumberTextbox").value);
+  var sum = firstNumber - secondNumber;
+  document.getElementById("resultTextboxid").value = sum.toString();
+
+}
+function multiplication()
+{
+    var firstNumber = parseInt(document.getElementById("firstnumberTextbox").value);//parse to integer, parseInt is a method, which will take the string as an input and convert them to integer.
+    var secondNumber = parseInt(document.getElementById("secondnumberTextbox").value);
+    var sum = firstNumber * secondNumber;
+    document.getElementById("resultTextboxid").value = sum.toString(); 
+}
+function division()
+{
+    var firstNumber = parseInt(document.getElementById("firstnumberTextbox").value);//parse to integer, parseInt is a method, which will take the string as an input and convert them to integer.
+  var secondNumber = parseInt(document.getElementById("secondnumberTextbox").value);
+  var sum = firstNumber / secondNumber;
+  document.getElementById("resultTextboxid").value = sum.toString();
+}
