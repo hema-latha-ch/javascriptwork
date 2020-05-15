33117ea313b9282b5d029fced47e7760d1e11559
hemalatha chintalapally <hemalatha.chintalapally@gmail.com>
2020-05-15 13:57:14 -0500

    removed unnesscessary code

--- find-greater-number/GreaterNumber.js
+++ find-greater-number/GreaterNumber.js
@@ -1,27 +1,17 @@
-window.addEventListener("load",start,false);
-//outout=20;
-function start()
-{
-    document.getElementById("submitbutton").addEventListener("click",greater,false);
-}
 function greater()
 {
     var firstnumber = parseInt(document.getElementById("firstinputnumber").value);
-    console.log(firstnumber)
     var secondnumber = parseInt(document.getElementById("secondinputnumber").value);
-    console.log(secondnumber)
     var outputresult=0; 
     if(firstnumber>secondnumber)
     {
        // firstnumber = outputresult;
         outputresult=firstnumber;
-        console.log(" if output result " + outputresult)
     }
     else 
     {
         //secondnumber = outputresult;
         outputresult=secondnumber;
-        console.log(" else output result " + outputresult)
     }
     document.getElementById("thirdoutputnumber").value = outputresult;
 }
--- hello-world-program/firstprogram.js
+++ hello-world-program/firstprogram.js
@@ -1,9 +1,3 @@
-window.addEventListener("load",start, false);
-function start()
-{
-	document.getElementById("submitbutton").addEventListener("click", displayMessage, false);
-	
-}
 function displayMessage()
 {
 	document.write("Welcome to programming world");
