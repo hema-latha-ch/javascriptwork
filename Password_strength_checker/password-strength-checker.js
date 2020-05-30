f1fb0e45fa684cfcc763584d49f71c12cbdf6647
hemalatha chintalapally <hemalatha.chintalapally@gmail.com>
2020-05-29 23:15:22 -0500

    password strength checker

---
+++ Password_strength_checker/password-strength-checker.js
@@ -0,0 +1,17 @@
+function loginClick()
+{
+    var email = document.getElementById("emailidtextbox").value;
+    var inputpassword = document.getElementById("passwordtextbox").value;
+
+    (isLoginSuccess(email,inputpassword))? alert("User Login successfully") : alert("Please enter valid email and password");
+    
+}
+function isLoginSuccess(email,inputpassword)
+{
+    return (email == "hemalatha@abc.com" && inputpassword == "abcd1234")?  true:  false;
+}
+function resetValues()
+{
+    document.getElementById("emailidtextbox").value = "";
+    document.getElementById("passwordtextbox").value = "";
+}
