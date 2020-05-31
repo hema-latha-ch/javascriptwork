215c17ce20cb5b1910f828111f3bdaafe50093e4
hemalatha chintalapally <hemalatha.chintalapally@gmail.com>
2020-05-30 23:40:26 -0500

    password strength checker

---
+++ Array-example/Arrayexamples.js
@@ -0,0 +1,8 @@
+var myarray = ["hema","sandeep","hema","sandeep","sandeep"];
+for(i=0;i<=myarray.length;i++)
+{
+    if(myarray[i] == "hema")
+    {
+        console.log("display"+myarray[i])
+    }
+}
